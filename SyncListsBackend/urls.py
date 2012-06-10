d9175fac779baf8f06f3c1dead6f3b76a3c7a6da
bbjay <bbjay@here.local>
2012-06-10 22:04:42 +0200

    use gunicorn for serving

--- SyncListsBackend/urls.py
+++ SyncListsBackend/urls.py
@@ -1,19 +1,21 @@
 from django.conf.urls import patterns, include, url
 import settings
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'SyncListsBackend.views.home', name='home'),
-    # url(r'^SyncListsBackend/', include('SyncListsBackend.foo.urls')),
+	# Examples:
+	# url(r'^$', 'SyncListsBackend.views.home', name='home'),
+	# url(r'^SyncListsBackend/', include('SyncListsBackend.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+	# Uncomment the next line to enable the admin:
+	# url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', redirect_to, {'url': '/index.html'}),
+	url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 )
