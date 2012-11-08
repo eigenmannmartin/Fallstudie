7dee0e43e7f9dd434c5882d489abbfbe2b570220
Martin Eigenmann <eim@WETCLT50165.loepfe.com>
2012-11-08 18:57:36 +0100

    some changes

--- SyncListsBackend/urls.py
+++ SyncListsBackend/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 import settings
 from django.views.generic.simple import redirect_to
 
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	# Examples:
@@ -12,10 +13,10 @@ urlpatterns = patterns('',
 	# url(r'^SyncListsBackend/', include('SyncListsBackend.foo.urls')),
 
 	# Uncomment the admin/doc line below to enable admin documentation:
-	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 	# Uncomment the next line to enable the admin:
-	# url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', redirect_to, {'url': '/index.html'}),
-	url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+	url(r'^admin/', include(admin.site.urls)),
+	#url(r'^$', redirect_to, {'url': '/index.html'}),
+	#url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
 )
