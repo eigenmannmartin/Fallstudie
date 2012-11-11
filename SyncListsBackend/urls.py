4fd4bb321e83435f298704b545710af2dc21e68d
bbjay <bbjay@here.local>
2012-11-11 18:12:31 +0100

    use django static serving with debug=false

--- SyncListsBackend/urls.py
+++ SyncListsBackend/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import redirect_to
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,6 +19,7 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 
 	url(r'^api/', include('SyncedList.urls_api')),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
+
 	url(r'^$', redirect_to, {'url': '/static/index.html'}),
 )
-urlpatterns += staticfiles_urlpatterns()
