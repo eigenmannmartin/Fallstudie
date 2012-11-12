e21c80d15f34e3e4fff77402dbda41ec051377cf
bbjay <bbjay@here.local>
2012-11-12 20:30:18 +0100

    declare DATABASES

--- SyncListsBackend/settings.py
+++ SyncListsBackend/settings.py
@@ -11,6 +11,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
