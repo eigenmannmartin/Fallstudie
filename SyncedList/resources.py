1b31c276248fe09e48f29728103c264910da9bde
Martin Eigenmann <eigenmannmartin@gmail.com>
2012-11-17 18:37:23 +0100

    local Auth

--- SyncedList/resources.py
+++ SyncedList/resources.py
@@ -39,11 +39,11 @@ class EntryResource(ModelResource):
 	def determine_format(self, request):
 		return "application/json" 
 
-	#def apply_authorization_limits(self, request, object_list):
-	#	return object_list.filter(list_read=request.user) | object_list.filter(list_write=request.user) | object_list.filter(list_owner=request.user)
+	def apply_authorization_limits(self, request, object_list):
+		return object_list.filter(entry_list__list_read=request.user) | object_list.filter(entry_list__list_write=request.user) |object_list.filter(entry_list__list_owner=request.user) 
 
 	class Meta:
-		queryset = ListEntry.objects.all()
+		queryset = ListEntry.objects.all().distinct()
 		authentication = EimAuthentication()
 		#authorization = DjangoAuthorization()
 		cache = SimpleCache()
@@ -63,13 +63,30 @@ class ListResource(ModelResource):
 	def is_autenticated(self,request):
 		return request.is_autenticated()
 
-	#list_entry = fields.ToManyField('EntryResource' , 'SyncedList.List.list_entry', full=True)
+	list_entry = fields.ToManyField(EntryResource, 'list_entry')
+	def prepend_urls(self):
+		return [
+			url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/entry%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_entry'), name="api_get_entry"),
+ 		]
+
+ 	def get_entry(self, request, **kwargs):
+		try:
+			obj = self.cached_obj_get(request=request, **self.remove_api_resource_names(kwargs))
+		except ObjectDoesNotExist:
+			return HttpGone()
+		except MultipleObjectsReturned:
+			return HttpMultipleChoices("More than one resource is found at this URI.")
+
+		child_resource = ChildResource()
+		return child_resource.get_detail(request, parent_id=obj.pk)
+
 	class Meta:
 		queryset = List.objects.all().distinct()
 		authentication = EimAuthentication()
 		#authorization = DjangoAuthorization()
 		cache = SimpleCache()
 		resource_name = 'List'
+		limit = 2000
 				
 
 
