542976be510721f221c019ab3c2ea4b87d3260b8
Martin Eigenmann <eigenmannmartin@gmail.com>
2012-11-20 13:27:48 +0100

    Login with Get and Post

--- SyncedList/models.py
+++ SyncedList/models.py
@@ -3,19 +3,23 @@ from django.db import models
 from django.contrib.auth.models import User
 # Var
 from datetime import datetime
-#from django.db.models import Q
 
 
 class ListEntry(models.Model):
+	entry_title = models.CharField(max_length=80)
 	entry_description = models.CharField(max_length=200)
 	entry_content = models.CharField(max_length=2000)
+	# state = in progress or done
 	entry_state = models.BooleanField()
+	# we do not delete, just mark them as deleted
 	entry_erased = models.BooleanField()
-	entry_title = models.CharField(max_length=80)
+	# to do until
 	entry_expiration = models.DateTimeField()
 	# creator - just for fun
 	create_date = models.DateTimeField(default=datetime.now())
 	create_user = models.ForeignKey(User, related_name="create_user") 
+
+	entry_version = models.IntegerField()
 
 	def __unicode__(self):
 		return self.entry_title
@@ -25,25 +29,21 @@ class ListEntry(models.Model):
 class List(models.Model):
 	list_name = models.CharField(max_length=40)
 	# allowed to edit the list
-	list_write = models.ManyToManyField(User, related_name="list_write")
-	list_owner = models.ManyToManyField(User, related_name="list_owner") 
+	list_write = models.ManyToManyField(User, related_name="list_write", null=True, blank=True)
+	list_owner = models.ManyToManyField(User, related_name="list_owner", null=True, blank=True) 
 	# allowed to view the list
-	list_read = models.ManyToManyField(User, related_name="list_read")
+	list_read = models.ManyToManyField(User, related_name="list_read", null=True, blank=True)
 	# creator - just for fun
 	create_date = models.DateTimeField(default=datetime.now())
 	create_user = models.ForeignKey(User) 
 
-	list_entry = models.ManyToManyField('ListEntry', related_name="entry_list")
+	list_entry = models.ManyToManyField('ListEntry', related_name="entry_list", null=True, blank=True)
+
+	list_version = models.IntegerField()
 
 	def __unicode__(self):
 		return self.list_name
 	
-
-class ListEntrySync(models.Model):
-	sync_listentry = models.ForeignKey('ListEntry')
-	sync_date = models.DateTimeField(default=datetime.now())
-	sync_user = models.ForeignKey(User, related_name="sync_user")
-
 
 
     
--- SyncedList/views_um.py
+++ SyncedList/views_um.py
@@ -2,31 +2,68 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
+from django.utils import simplejson
+
 
 
 
 def um_login(request):
-
-	user = authenticate(username='admin', password='admin')
-	if user is not None:
-	    if user.is_active:
-	    	login(request, user)
-	        out="You provided a correct username and password!"
-	    else:
-	        out="Your account has been disabled!"
+	response_data = {
+		"message" : "",
+		"result" : "",
+		"id" : "",
+	}
+	if request.method == 'GET':
+		username = request.GET.get('username', None)
+		password = request.GET.get('password', None)
+	elif request.method == 'POST':
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+	
+	if username is not None and password is not None:
+		user = authenticate(username=username, password=password)
 	else:
-	    out="Your username and password were incorrect."
+		user = None
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			response_data['message'] = "You provided a correct username and password!"
+			response_data['result'] = 'success'
+			response_data['id'] = "0"
+		else:
+			response_data['message'] = "Your account has been disabled!"
+			response_data['result'] = 'failed'
+			response_data['id'] = "-1"
+	else:
+		response_data['message'] = "Your username or password were incorrect."
+		response_data['result'] = 'failed'
+		response_data['id'] = "-2"
 
 
-	return HttpResponse(out)
+	return HttpResponse(simplejson.dumps(response_data), mimetype="application/json")
 
 def um_user(request):
+	response_data = {
+		"message" : "",
+		"result" : "",
+		"id" : "",
+	}
 	if request.user.is_authenticated():
-		out = request.user.username
+		response_data['message'] = request.user.username
+		response_data['result'] = 'success'
+		response_data['id'] = "0"
 	else:
-		out = "sorry, you are not logged in"
-	return HttpResponse(out)
+		response_data['message'] = "sorry, you are not logged in"
+		response_data['result'] = 'failed'
+		response_data['id'] = "-1"
+
+	return HttpResponse(simplejson.dumps(response_data), mimetype="application/json")
 
 def um_logout(request):
+	response_data = {
+		"message" : "Successfully logged out!",
+		"result" : "success",
+		"id" : "0",
+	}
 	logout(request)
-	return HttpResponse("Successfully logged out")
+	return HttpResponse(simplejson.dumps(response_data), mimetype="application/json")
