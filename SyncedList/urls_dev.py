b58e6476ca0240fd0be7f59e61cb357b9cebcaf8
Martin Eigenmann <eigenmannmartin@gmail.com>
2012-11-21 08:27:10 +0100

    login Form

--- SyncedList/urls_dev.py
+++ SyncedList/urls_dev.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('',
-
+	url(r'^$', 'SyncedList.views_dev.index'),
 )
---
+++ SyncedList/views_dev.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
+
+def index(request):
+	c = {}
+	c.update(csrf(request))
+	return render_to_response("login.html", c)
